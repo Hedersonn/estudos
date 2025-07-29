5053598918f4151926d811c16a884bc5e967f0d3
Hedersonn <rigonatti.hederson@gmail.com>
2025-07-29 17:21:05 -0300

    Adicionando primeira aula do segundo curso de POO python

---
+++ alura/python/sabor-express-app/exercicios/2.1-classes/ex01.py
@@ -0,0 +1,23 @@
+# exercicio 1 - Crie uma classe chamada Musica com os seguintes atributos(nome, artista, duracao) e crie 3 objetos definindo cada atributo..
+
+class Musica:
+    nome = ""
+    artista = ""
+    duracao = int
+
+
+musica1 = Musica()
+musica1.nome = "Lose Yourself"
+musica1.artista = "Eminem"
+musica1.duracao = 326
+
+musica2 = Musica()
+musica2.nome = "Easy on me"
+musica2.artista = "Adele"
+musica2.duracao = 224
+
+musica3 = Musica()
+musica3.nome = "Halo"
+musica3.artista = "Beyonce"
+musica3.duracao = 225
+
