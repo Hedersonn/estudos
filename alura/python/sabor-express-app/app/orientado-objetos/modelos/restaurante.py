34cdc4cea2d850d0f00648b4f986f4b9c7929cb0
Hedersonn <rigonatti.hederson@gmail.com>
2025-07-30 19:16:55 -0300

    Finalizando aula 2 do curso 2

--- alura/python/sabor-express-app/app/orientado-objetos/modelos/restaurante.py
+++ alura/python/sabor-express-app/app/orientado-objetos/modelos/restaurante.py
@@ -1,11 +1,21 @@
-class Restaurante: 
-    nome = ""
-    categoria = ""
-    ativo = False
+class Restaurante:
+    restaurantes = []
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
 
-restaurante_praca = Restaurante()
-restaurante_pizza = Restaurante()
 
-restaurante_praca.categoria = "Gourmet"
-restaurante_praca.nome = "Praça"
-print(vars(restaurante_praca))
+    def __str__(self):
+        return f"{self.nome} | {self.categoria}"
+    
+
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f"{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}")
+
+restaurante_praca = Restaurante("Praça", "Gourmet")
+restaurante_pizza = Restaurante("Pizza Express", "Italiana")
+
+Restaurante.listar_restaurantes()
