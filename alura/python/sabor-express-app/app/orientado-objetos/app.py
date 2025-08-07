2c10d8ca6d0ead52d488cef6b33eacce478af046
rigonatti.hederson@gmail.com <rigonatti.hederson@gmail.com>
2025-08-07 17:45:39 -0300

    Adicionando medodo str nas classes do cardapio

--- alura/python/sabor-express-app/app/orientado-objetos/app.py
+++ alura/python/sabor-express-app/app/orientado-objetos/app.py
@@ -1,12 +1,15 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante("Praça", "Gourmet")
-restaurante_praca.avaliar("Gui", 10)
-restaurante_praca.avaliar("Lais", 8)
-restaurante_praca.avaliar("Emy", 2)
+bebida_suco = Bebida("Melancia", 5.0, "grande")
+prato_paozinho = Prato("Paozinho", 2.0, "o melhor pão da cidade")
+
 
 def main():
-    Restaurante.listar_restaurantes()
+	print(bebida_suco)
+	print(prato_paozinho)
 
 if __name__ == "__main__":
     main()
--- alura/python/sabor-express-app/app/orientado-objetos/modelos/cardapio/prato.py
+++ alura/python/sabor-express-app/app/orientado-objetos/modelos/cardapio/prato.py
@@ -4,3 +4,7 @@ class Prato(ItemCardapio):
 	def __init__(self, nome, preco, descricao):
 		super().__init__(nome, preco)
 		self.descricao = descricao
+
+
+	def __str__(self):
+		return self.nome
