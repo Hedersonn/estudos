911450e691bb195b106b652c8ac0af9a6020655c
Hedersonn <rigonatti.hederson@gmail.com>
2025-08-12 18:04:08 -0300

    Colocando enunciados nos exercícios do curso 3

--- alura/python/sabor-express-app/exercicios/3.1-heranca/ex02.py
+++ alura/python/sabor-express-app/exercicios/3.1-heranca/ex02.py
@@ -1,3 +1,11 @@
+# Crie uma Classe Pai (Veiculo): Implemente uma classe chamada Veiculo com um construtor que aceita dois parâmetros, marca e modelo. A classe deve ter um atributo protegido _ligado inicializado como False por padrão.
+# Construa o Método Especial str: Adicione um método especial str à classe Veiculo que retorna uma mensagem formatada com a marca, modelo e o estado de ligado/desligado do veículo.
+# Crie uma Classe Filha (Carro): Agora, crie uma classe chamada Carro que herda da classe Veiculo. No construtor da classe Carro, inclua um novo atributo chamado portas que indica a quantidade de portas do carro.
+# Implemente o Método Especial str na Classe Filha: Adicione um método especial str à classe Carro que estenda o método da classe pai (Veiculo) e inclua a informação sobre a quantidade de portas do carro.
+# Crie uma Classe Filha (Moto): Similarmente, crie uma classe chamada Moto que também herda de Veiculo. Adicione um novo atributo chamado tipo ao construtor, indicando se a moto é esportiva ou casual.
+# Implemente o Método Especial str na Classe Filha (Moto): Adicione um método especial str à classe Moto que estenda o método da classe pai (Veiculo) e inclua a informação sobre o tipo da moto.
+# Exiba as Informações: Para cada instância, imprima no console as informações utilizando o método str.
+
 class Veiculo:
     def __init__(self, marca, modelo):
         self.marca = marca
