05863d2e9e320c9b0a029f7ebbb5b79f7d19c5a1
Hedersonn <rigonatti.hederson@gmail.com>
2025-08-13 15:14:15 -0300

    Organizando dados do restaurante com lista

--- alura/python/sabor-express-app/app/ambiente-virtual/app.py
+++ alura/python/sabor-express-app/app/ambiente-virtual/app.py
@@ -6,6 +6,20 @@ print(response)
 
 if response.status_code == 200:
     dados_json = response.json()
-    print(dados_json)
+    dados_restaurante = {}
+
+    for item in dados_json:
+        nome_restaurante = item["Company"]
+        if nome_restaurante not in dados_restaurante:
+            dados_restaurante[nome_restaurante] = []
+
+        dados_restaurante[nome_restaurante].append({
+            "item": item["Item"], 
+            "preco": item["price"],
+            "descricao": item["description"]
+        })
 else:
     print(f"O erro foi {response.status_code}")
+
+
+print(dados_restaurante)
