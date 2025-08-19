f2fc36e7fec976fd7a068684dd84a7653528fe9e
Hedersonn <rigonatti.hederson@gmail.com>
2025-08-19 18:13:51 -0300

    Adicionando novas condições do número secreto

--- alura/javascript/logica-programacao/jogo-adivinhacao/app.js
+++ alura/javascript/logica-programacao/jogo-adivinhacao/app.js
@@ -7,5 +7,9 @@ let chute = prompt("Escolha um número entre 1 e 10");
 if (chute == numeroSecreto) {
     alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
 } else {
-    alert("Você errrou! :(");
+    if (numeroSecreto > chute) {
+        alert("O número secreto é maior.");
+    } else {
+        alert("O número secreto é menor.");
+    }
 }
