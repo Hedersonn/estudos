17f063390b94f0fc4f02514ed823bfc229be34c3
Hedersonn <rigonatti.hederson@gmail.com>
2025-09-02 14:58:02 -0300

    Iniciando e completando exercicios em java

---
+++ alura/java/exercicios/ex02/Perfil/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome = "Hederson";
+        System.out.println("Olá " + nome);
+        System.out.println("Tudo bem??");
+    }
+}
+
