92033486d76e7eba49312f687c21fcb06fc7afd4
Hedersonn <rigonatti.hederson@gmail.com>
2025-09-02 18:29:28 -0300

    Finalizando aula 2 curso 2 java

--- alura/java/screen-match-app/app/src/Main.java
+++ alura/java/screen-match-app/app/src/Main.java
@@ -7,6 +7,16 @@ public class Main {
         System.out.println("Ano de lançamento: " + anoDeLancamento);
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
+
+        double media = 8.3 + 6.3 + 8.0 / 3;
+        System.out.println(media);
+
+        String sinopse;
+        sinopse = "Filme de ação com um cara forte";
+        System.out.println(sinopse);
+
+        int classificacao = (int) (media / 2);
+        System.out.println(classificacao);
         
     }
 }
