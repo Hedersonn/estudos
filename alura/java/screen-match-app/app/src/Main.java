4931af13a7af79a686823c24c818073b9f0d4b8d
Hedersonn <rigonatti.hederson@gmail.com>
2025-09-02 16:59:34 -0300

    Entendendo e removendo arquivos

--- alura/java/screen-match-app/app/src/Main.java
+++ alura/java/screen-match-app/app/src/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Este é o Screen Match");
+        System.out.println("Filme: Rambo");
+
+        int anoDeLancamento = 2008;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        
+    }
 }
