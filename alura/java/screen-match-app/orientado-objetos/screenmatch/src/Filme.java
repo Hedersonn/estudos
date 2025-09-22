faae575316a36ad7a0b83c75a607475c3878315e
Hedersonn <rigonatti.hederson@gmail.com>
2025-09-22 19:33:39 -0300

    Iniciando estudos de OO

--- alura/java/screen-match-app/orientado-objetos/screenmatch/src/Filme.java
+++ alura/java/screen-match-app/orientado-objetos/screenmatch/src/Filme.java
@@ -1,2 +1,23 @@
 public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double avaliacao;
+    int totalAvaliacoes;
+    int duracao;
+
+    void exibeFichaTecnica(){
+        System.out.println(nome);
+        System.out.println(anoDeLancamento);
+        System.out.println(incluidoNoPlano);
+    }
+    void avalia(double nota){
+        avaliacao += nota;
+        totalAvaliacoes++;
+    }
+
+    double calcularMedia(){
+        return avaliacao / totalAvaliacoes;
+    }
+
 }
--- alura/java/screen-match-app/orientado-objetos/screenmatch/src/Principal.java
+++ alura/java/screen-match-app/orientado-objetos/screenmatch/src/Principal.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O Poderoso Chefão";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracao = 180;
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(3);
+        meuFilme.avalia(5);
+        meuFilme.avalia(8);
+        System.out.println(meuFilme.avaliacao);
+        System.out.println(meuFilme.totalAvaliacoes);
+        System.out.println(meuFilme.calcularMedia());
+
+    }
 }
