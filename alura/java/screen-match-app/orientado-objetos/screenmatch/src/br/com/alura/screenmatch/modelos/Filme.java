634dc8b094ac38a1062b2a0d348ef15707f02160
Hedersonn <rigonatti.hederson@gmail.com>
2025-09-24 20:08:15 -0300

    Entendendo modificadores de acesso e Getters/Setters

---
+++ alura/java/screen-match-app/orientado-objetos/screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,61 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Filme {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaAvaliacoes;
+    private int totalAvaliacoes;
+    private int duracao;
+
+    public int getTotalAvaliacoes(){
+        return totalAvaliacoes;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public void exibeFichaTecnica(){
+        System.out.println(nome);
+        System.out.println(anoDeLancamento);
+        System.out.println(incluidoNoPlano);
+    }
+    public void avalia(double nota){
+        somaAvaliacoes += nota;
+        totalAvaliacoes++;
+    }
+
+    public double calcularMedia(){
+        return somaAvaliacoes / totalAvaliacoes;
+    }
+
+}
