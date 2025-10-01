55824cded6fd6e54701cbc06ba3c785695ed2af6
Hedersonn <rigonatti.hederson@gmail.com>
2025-10-01 19:05:32 -0300

    Adicionando classe mãe das subclasses filme e serie

--- alura/java/screen-match-app/orientado-objetos/screenmatch/src/Principal.java
+++ alura/java/screen-match-app/orientado-objetos/screenmatch/src/Principal.java
@@ -1,4 +1,6 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -13,5 +15,24 @@ public class Principal {
         meuFilme.avalia(8);
         System.out.println("Total de Avaliações: " + meuFilme.getTotalAvaliacoes());
         System.out.println(meuFilme.calcularMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodios(50);
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracao(200);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(lost);
+        System.out.println(calculadora.getTempoTotal());
     }
 }
--- alura/java/screen-match-app/orientado-objetos/screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
+++ alura/java/screen-match-app/orientado-objetos/screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,61 +1,13 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme {
-    private String nome;
-    private int anoDeLancamento;
-    private boolean incluidoNoPlano;
-    private double somaAvaliacoes;
-    private int totalAvaliacoes;
-    private int duracao;
+public class Filme extends Titulo{
+  private String diretor;
 
-    public int getTotalAvaliacoes(){
-        return totalAvaliacoes;
+    public String getDiretor() {
+        return diretor;
     }
 
-    public String getNome() {
-        return nome;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
-
-    public int getAnoDeLancamento() {
-        return anoDeLancamento;
-    }
-
-    public boolean isIncluidoNoPlano() {
-        return incluidoNoPlano;
-    }
-
-    public int getDuracao() {
-        return duracao;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    public void setDuracao(int duracao) {
-        this.duracao = duracao;
-    }
-
-    public void exibeFichaTecnica(){
-        System.out.println(nome);
-        System.out.println(anoDeLancamento);
-        System.out.println(incluidoNoPlano);
-    }
-    public void avalia(double nota){
-        somaAvaliacoes += nota;
-        totalAvaliacoes++;
-    }
-
-    public double calcularMedia(){
-        return somaAvaliacoes / totalAvaliacoes;
-    }
-
 }
