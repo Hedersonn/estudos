4598b1ee34c82284c3be2a7cd7fb932dff165945
Hedersonn <rigonatti.hederson@gmail.com>
2025-11-12 15:37:20 -0300

    Aidicionando contrutor na classe super(Titulo) e nas classes filhas

--- alura/java/screen-match-app/orientado-objetos/screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
+++ alura/java/screen-match-app/orientado-objetos/screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -5,8 +5,8 @@ import br.com.alura.screenmatch.calculos.Classificacao;
 public class Filme extends Titulo implements Classificacao {
   private String diretor;
 
-    public Filme(String nome) {
-        this.setNome(nome);
+    public Filme(String nome, int anoDeLancamento) {
+        super(nome, anoDeLancamento);
     }
 
     public String getDiretor() {
